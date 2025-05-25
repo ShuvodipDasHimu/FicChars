56b4d4bc330e34c54f455e393a3f8a4b448b2efb
Shuvodip Das Himu <shuvodipdashimu@gmail.com>
2025-05-25 20:29:21 +0600

    adjusted padding for fav3 individual cards

--- src/components/FavThree.jsx
+++ src/components/FavThree.jsx
@@ -6,7 +6,7 @@ const FavThree = () => {
     <div className="bg-slate-200 py-24 flex flex-col items-center">
       <h1 className="text-3xl font-bold text-center mb-12 text-gray-800">My Fav 3 Characters out of the Big 3 Anime</h1>
       <div className="flex gap-16 items-center justify-center flex-wrap">
-        <div className="flex flex-col items-center py-10 px-4  rounded-lg  shadow-xl hover:bg-purple-100 hover: bg-purple-200 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
+        <div className="flex flex-col items-center py-10 px-8  rounded-2xl shadow-xl hover:bg-purple-100 hover: bg-purple-200 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
           <img
             className="w-80 h-[350px] rounded-lg shadow-lg"
             src="https://i.postimg.cc/vB78qfqd/fav3aizen.webp"
@@ -25,7 +25,7 @@ const FavThree = () => {
             className="mt-4 text-3xl font-mono text-purple-600"
           />
         </div>
-        <div className="flex flex-col items-center py-10 px-4  rounded-lg  shadow-xl hover:bg-red-100 bg-red-200 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
+        <div className="flex flex-col items-center py-10 px-8  rounded-2xl shadow-xl hover:bg-red-100 bg-red-200 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
           <img
             className="w-80 h-[350px] rounded-lg shadow-lg"
             src="https://i.postimg.cc/W3cbwX7n/fav3itachi.webp"
@@ -44,7 +44,7 @@ const FavThree = () => {
             className="mt-4 text-3xl font-mono text-red-600"
           />
         </div>
-        <div className="flex flex-col items-center py-10 px-4  rounded-lg  shadow-xl hover:bg-green-50 bg-green-100 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
+        <div className="flex flex-col items-center py-10 px-8  rounded-2xl shadow-xl hover:bg-green-50 bg-green-100 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
           <img
             className="w-80 h-[350px] rounded-lg shadow-lg"
             src="https://i.postimg.cc/Pqw5DB1J/fav3zoro.webp"
